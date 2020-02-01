bfdd95a20a823da6b082290a814796ba14c1671d
rina.sakamaki <rina.sakamaki@191201M02noMacBook-Pro.local>
2020-02-01 18:37:52 +0900

    add function to button on startpage

---
+++ GGJ2020/Assets/Resources/Scripts/StartGameButton.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartGameButton : MonoBehaviour
+{
+    public GameObject Parent;
+
+    public void OnClickStartButton ()
+    {
+        Parent.SetActive(false);
+    }
+
+}
