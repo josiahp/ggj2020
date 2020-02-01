80f42d6367faead2f60b19e31f40eba52c9cf1be
rina.sakamaki <rina.sakamaki@191201M02noMacBook-Pro.local>
2020-02-01 23:17:48 +0900

    add gamestates

--- GGJ2020/Assets/Resources/Scripts/GameController.cs
+++ GGJ2020/Assets/Resources/Scripts/GameController.cs
@@ -5,10 +5,24 @@ using UnityEngine;
 public class GameController : MonoBehaviour
 {
     public GameObject StartPage;
+    public GameObject Timer;
     public float conveyorBeltSpeed = 1.0f;
     private float speedModifier = -0.5f;
 
     private List<GameObject> boxes = new List<GameObject>();
+
+    // public string gameState = "not started";
+    private GameStates gameState = GameStates.NOT_STARTED;
+    public enum GameStates : int{
+        NOT_STARTED = 0,
+        STARTED,
+        FINISHED
+    }
+
+    public void StartGame()
+    {
+        gameState = GameStates.STARTED;
+    }
 
     // Start is called before the first frame update
     void Start()
@@ -23,7 +37,6 @@ public class GameController : MonoBehaviour
         foreach (GameObject box in boxes.ToArray())
         {
             box.transform.position = box.transform.position + new Vector3(GetConveyerBeltSpeed() * Time.deltaTime, 0f, 0f);
-
         }
     }
 
@@ -43,6 +56,11 @@ public class GameController : MonoBehaviour
     public float GetConveyerBeltSpeed()
     {
         return conveyorBeltSpeed * speedModifier;
+    }
+
+    public GameStates GetGameState()
+    {
+        return gameState;
     }
 
 }
--- GGJ2020/Assets/Resources/Scripts/StartGameButton.cs
+++ GGJ2020/Assets/Resources/Scripts/StartGameButton.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class StartGameButton : MonoBehaviour
 {
     public GameObject Parent;
+    public GameObject GameController;
 
     public void OnClickStartButton ()
     {
         Parent.SetActive(false);
+        GameController.GetComponent<GameController>().StartGame();
     }
 
 }
