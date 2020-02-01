725c3ac5db52f770ca140395c045c25999f9b3e6
Josiah Purtlebaugh <josiah.purtlebaugh@gmail.com>
2020-02-01 23:19:19 +0900

    add new assets add placeholder scripts

---
+++ GGJ2020/Assets/Resources/Scripts/ButtonController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonController : MonoBehaviour
+{
+    public GameObject buttonPressed;
+    public GameObject buttonUnpressed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseDown()
+    {
+        Debug.Log("pressed");
+        if (buttonPressed.active)
+        {
+            Debug.Log("switched1!");
+            buttonPressed.SetActive(false);
+            buttonPressed.GetComponent<BoxCollider>().enabled = false;
+            buttonUnpressed.SetActive(true);
+            buttonUnpressed.GetComponent<BoxCollider>().enabled = true;
+        } else if (buttonUnpressed.active)
+        {
+            Debug.Log("switched2!");
+            buttonPressed.SetActive(true);
+            buttonPressed.GetComponent<BoxCollider>().enabled = true;
+            buttonUnpressed.SetActive(false);
+            buttonUnpressed.GetComponent<BoxCollider>().enabled = false;
+        }
+    }
+}
---
+++ GGJ2020/Assets/Resources/Scripts/LightPulse.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightPulse : MonoBehaviour
+{
+    Light lt;
+    float originalRange;
+    float duration = 1.0f;
+
+    private Material redLight;
+    private Material yellowLight;
+    private Material greenLight;
+
+    void Start()
+    {
+        lt = GetComponentInChildren<Light>();
+        originalRange = lt.range;
+
+        int rnd = Random.Range(0, 3);
+
+        if (rnd == 0)
+        {
+            redLight = Resources.Load<Material>("Materials/Red Light");
+            gameObject.GetComponent<MeshRenderer>().material = redLight;
+            lt.color = Color.red;
+        } else if (rnd == 1)
+        {
+            yellowLight = Resources.Load<Material>("Materials/Yellow Light");
+            gameObject.GetComponent<MeshRenderer>().material = yellowLight;
+            lt.color = Color.yellow;
+        }
+        else if (rnd == 2)
+        {
+            greenLight = Resources.Load<Material>("Materials/Green Light");
+            gameObject.GetComponent<MeshRenderer>().material = greenLight;
+            lt.color = Color.green;
+        }
+
+
+    }
+
+    void Update()
+    {
+        var amplitude = Mathf.PingPong(Time.time, duration);
+
+        // Transform from 0..duration to 0.5..1 range.
+        amplitude = amplitude / duration * 2f + 0.5f;
+
+        // Set light range.
+        lt.range = originalRange * amplitude;
+    }
+}
