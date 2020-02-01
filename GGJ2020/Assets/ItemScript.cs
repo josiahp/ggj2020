920b0036cc493e663cb1a6cbdde5229092a12863
Josiah Purtlebaugh <josiah.purtlebaugh@gmail.com>
2020-02-01 13:12:41 +0900

    add conveyor belt, lightbox, poster, main scene

---
+++ GGJ2020/Assets/ItemScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public GameObject light;
+
+    void Start()
+    { 
+        light.transform.GetComponent<Light>().color = Color.red;
+        light.transform.GetComponent<Renderer>().material.color = Color.red;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
